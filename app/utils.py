391a0ffc70a4a7e517c019a15beae5baef91bb47
riad7788 <mrriad7788@gmail.com>
2026-02-04 00:07:59 +0600

    Delete app directory

--- app/utils.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_URL = "http://127.0.0.1:8000/predict"
-
-def send_image(image):
-    files = {"file": image}
-    response = requests.post(API_URL, files=files)
-    return response.json()
